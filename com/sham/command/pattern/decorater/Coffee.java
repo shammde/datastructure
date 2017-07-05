30df7474d82ace37baaa6de1961042701f66befa
shammde <sham.mde@gmail.com>
2017-07-05 21:31:03 +0530

    Delete Coffee.java

--- com/sham/command/pattern/decorater/Coffee.java
+++
@@ -1,6 +0,0 @@
-package com.sham.command.pattern.decorater;
-
-public interface Coffee {
-    public double getCost(); // Returns the cost of the coffee
-    public String getIngredients(); // Returns the ingredients of the coffee
-}
