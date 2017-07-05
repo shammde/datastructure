5cecb5fd2f3507f52c69575902c6d972ded83e44
shammde <sham.mde@gmail.com>
2017-07-05 20:58:58 +0530

    Create Item.java

---
+++ com/sham/pattern/visitor/Item.java
@@ -0,0 +1,19 @@
+package com.sham.pattern.visitor;
+
+public abstract class Item {
+	
+	double price;
+	
+	public Item(double price) {
+		this.price = price;
+	}
+
+
+	public double getPrice() {
+		return price;
+	}
+
+	public void setPrice(double price) {
+		this.price = price;
+	}
+}
