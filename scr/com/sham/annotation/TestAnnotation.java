f23c8957e27ddf7e706446259831bcfc9a7b4c25
mehtesham <mehteshamn@vlocity.com>
2019-12-14 13:21:30 +0530

    Revert "Revert "Add few more changes""
    This reverts commit e7158bc36da09ac5eb456a212879ecaeca380247.

--- scr/com/sham/annotation/TestAnnotation.java
+++ scr/com/sham/annotation/TestAnnotation.java
@@ -5,7 +5,7 @@ import java.lang.reflect.Method;
 class TestAnnotation{  
 	public static void main(String args[])throws Exception{  
 
-		Hello h=new Hello();  
+		Hello h = new Hello();  
 		Method m=h.getClass().getMethod("sayHello");  
 
 		MyAnnotation manno=m.getAnnotation(MyAnnotation.class);  
